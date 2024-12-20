553970bf286870e6e536c6a155bcad75da700731
Osman <44681202+kilicosman@users.noreply.github.com>
2024-12-20 11:48:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase bağlantısı
-const SUPABASE_URL = 'your-supabase-url'; // .env dosyasından alınacak
-const SUPABASE_ANON_KEY = 'your-anon-key'; // .env dosyasından alınacak
+const SUPABASE_URL = 'https://xgawgxnzmhhhfrlambzq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnYXdneG56bWhoaGZybGFtYnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2ODgzNjYsImV4cCI6MjA1MDI2NDM2Nn0.clUilHcXBAU3MCttysmdrIgudfgOPZJV-nSIWVWH-Eg';
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Şifre ve diğer sabitler

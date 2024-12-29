2bc57a4b732b8b5a9753946b4b3149c4e450d3d4
Osman <44681202+kilicosman@users.noreply.github.com>
2024-12-29 23:32:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const port = process.env.PORT || 3000;
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/login', async (req, res) => {

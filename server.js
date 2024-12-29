a6e42133849a21a05c1348f20b5f2f870238dc23
Osman <44681202+kilicosman@users.noreply.github.com>
2024-12-29 20:42:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANO
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Ortak HAtim');
+});
+
 app.post('/login', async (req, res) => {
     const { password } = req.body;
     if (password === 'vefa') {

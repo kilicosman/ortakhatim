33d69123135e57334bf2166a4515663a3e566cf9
Osman <44681202+kilicosman@users.noreply.github.com>
2025-01-03 10:06:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname));  // Serve static files from the root directory
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html','style.css','script.js'));
 });
 
 app.post('/login', async (req, res) => {

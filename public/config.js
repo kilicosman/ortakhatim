0fc46eba43af1e790b05b1c65013284e951051ed
Osman <44681202+kilicosman@users.noreply.github.com>
2025-01-28 10:51:59 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-    SUPABASE_URL: 'your_supabase_url',
-    SUPABASE_ANON_KEY: 'your_supabase_anon_key'
+    SUPABASE_URL = 'https://xgawgxnzmhhhfrlambzq.supabase.co';
+SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnYXdneG56bWhoaGZybGFtYnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2ODgzNjYsImV4cCI6MjA1MDI2NDM2Nn0.clUilHcXBAU3MCttysmdrIgudfgOPZJV-nSIWVWH-Eg'
 };
